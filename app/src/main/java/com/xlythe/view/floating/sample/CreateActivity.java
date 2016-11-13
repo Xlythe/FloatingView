6946b55190eb27379f04b5e236d3a8d575ddd0e5
Will <xlythe@gmail.com>
2016-11-12 19:14:32 -0800

    add shortcut activities to library

--- app/src/main/java/com/xlythe/view/floating/sample/CreateActivity.java
+++ app/src/main/java/com/xlythe/view/floating/sample/CreateActivity.java
@@ -1,59 +1,21 @@
 package com.xlythe.view.floating.sample;
 
-import android.app.Activity;
-import android.content.Intent;
-import android.net.Uri;
-import android.os.Build;
-import android.os.Bundle;
-import android.provider.Settings;
+import android.support.annotation.DrawableRes;
+
+import com.xlythe.view.floating.CreateShortcutActivity;
 
 /**
  * Creates the shortcut icon
  */
-public class CreateActivity extends Activity {
-    private static final int REQUEST_CODE_WINDOW_OVERLAY_PERMISSION = 10001;
-
-    public void onCreate(Bundle state) {
-        super.onCreate(state);
-
-        if (Intent.ACTION_CREATE_SHORTCUT.equals(getIntent().getAction())) {
-            if (android.os.Build.VERSION.SDK_INT >= 23 && !Settings.canDrawOverlays(this)) {
-                startActivityForResult(
-                        new Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION, Uri.parse("package:" + getPackageName())),
-                        REQUEST_CODE_WINDOW_OVERLAY_PERMISSION);
-            } else {
-                onSuccess();
-            }
-        }
-    }
-
-    private void onSuccess() {
-        Intent.ShortcutIconResource icon = Intent.ShortcutIconResource.fromContext(this, R.mipmap.ic_launcher);
-
-        Intent intent = new Intent();
-        Intent launchIntent = new Intent(this, OpenActivity.class);
-
-        intent.putExtra(Intent.EXTRA_SHORTCUT_INTENT, launchIntent);
-        intent.putExtra(Intent.EXTRA_SHORTCUT_NAME, getString(R.string.app_name));
-        intent.putExtra(Intent.EXTRA_SHORTCUT_ICON_RESOURCE, icon);
-
-        setResult(RESULT_OK, intent);
-        finish();
-    }
-
-    private void onFailure() {
-        setResult(RESULT_CANCELED);
-        finish();
-    }
-
+public class CreateActivity extends CreateShortcutActivity {
     @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if (requestCode == REQUEST_CODE_WINDOW_OVERLAY_PERMISSION) {
-            if (Build.VERSION.SDK_INT >= 23 && Settings.canDrawOverlays(this)) {
-                onSuccess();
-            } else {
-                onFailure();
-            }
-        }
+    public CharSequence getShortcutName() {
+        return getString(R.string.app_name);
+    }
+
+    @DrawableRes
+    @Override
+    public int getShortcutIcon() {
+        return R.mipmap.ic_launcher;
     }
 }
---
+++ floating-view/src/main/java/com/xlythe/view/floating/OpenShortcutActivity.java
@@ -0,0 +1,56 @@
+package com.xlythe.view.floating;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.Settings;
+
+/**
+ * When the shortcut icon is pressed, use this Activity to launch the overlay Service
+ */
+public abstract class OpenShortcutActivity extends Activity {
+    private static final int REQUEST_CODE_WINDOW_OVERLAY_PERMISSION = 10001;
+
+    public abstract Intent createServiceIntent();
+
+    public void onCreate(Bundle state) {
+        super.onCreate(state);
+
+        if (android.os.Build.VERSION.SDK_INT >= 23 && !Settings.canDrawOverlays(this)) {
+            startActivityForResult(
+                    new Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION, Uri.parse("package:" + getPackageName())),
+                    REQUEST_CODE_WINDOW_OVERLAY_PERMISSION);
+        } else {
+            onSuccess();
+        }
+    }
+
+    private void onSuccess() {
+        Intent intent = createServiceIntent();
+        startService(intent);
+        finish();
+    }
+
+    private void onFailure() {
+        setResult(RESULT_CANCELED);
+        finish();
+    }
+
+    @Override
+    public void finish() {
+        super.finish();
+        overridePendingTransition(R.anim.blank, R.anim.blank);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_CODE_WINDOW_OVERLAY_PERMISSION) {
+            if (android.os.Build.VERSION.SDK_INT >= 23 && Settings.canDrawOverlays(this)) {
+                onSuccess();
+            } else {
+                onFailure();
+            }
+        }
+    }
+}
